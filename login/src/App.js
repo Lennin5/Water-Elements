2352afae9a0fed1c2fca14a577eb59ca35fdcb32
Rodrigo Caballero <rcaballero082@gmail.com>
2021-09-08 04:34:02 -0600

    Inicio del Login en React, la vista debe pulirse

---
+++ login/src/App.js
@@ -0,0 +1,24 @@
+import './style.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Login</h1>
+  <div class="textbox">
+    <i class="fas fa-user"></i>
+    <input type="text" placeholder="Username" id="email"></input>
+  </div>
+
+  <div class="textbox">
+    <i class="fas fa-lock"></i>
+    <input type="password" placeholder="Password" id="pass"></input>
+  </div>
+  <br></br>
+  
+  <input type="button" class="btn" value="Sign in" onclick="preview()"></input>
+    </div>
+  );
+}
+
+export default App;
